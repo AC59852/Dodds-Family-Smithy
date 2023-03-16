3a2d2c6b08244dc678d648a82b4c38f9247158fd
AC59852 <austin.caron1@gmail.com>
2023-03-16 15:04:31 -0400

    added desktop styles for cart page

---
+++ components/CartFormComponent.vue
@@ -0,0 +1,98 @@
+<template>
+  <form action="" class="cart__form">
+      <div class="cartForm__wrapper">
+        <label for="name" class="cartForm__label">Name</label>
+        <input type="text" name="name" placeholder="Name (Required)" id="name" required class="cartForm__input cartForm__input--name">
+      </div>
+      <div class="cartForm__wrapper">
+        <label for="email" class="cartForm__label">Email</label>
+        <input type="email" placeholder="Email (Required)" name="email" id="email" required class="cartForm__input cartForm__input--email">
+      </div>
+      <div class="cartForm__wrapper">
+        <label for="address" class="cartForm__label">Address</label>
+        <input type="text" placeholder="Address (Required)" name="address" id="address" required autocomplete="address" class="cartForm__input cartForm__input--address">
+      </div>
+      <div class="cartForm__wrapper">
+        <label for="city" class="cartForm__label">City</label>
+        <input type="text" name="city" placeholder="City (Required)" id="city" required autocomplete="city" class="cartForm__input cartForm__input--city">
+      </div>
+      <div class="cartForm__wrapper">
+        <label for="message" class="cartForm__label">Message</label>
+        <textarea name="message" id="message" cols="30" maxlength="300" rows="10" class="cartForm__input cartForm__input--message" placeholder="Message (Optional)"></textarea>
+      </div>
+      <button type="submit" class="cartForm__submit">Place Request</button>
+    </form>
+</template>
+<style scoped>
+.cart__form {
+  width: 100%;
+  max-width: 500px;
+  margin-left: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.cartForm__wrapper {
+  display: flex;
+  flex-direction: column;
+}
+
+.cartForm__label {
+  display: none;
+}
+
+.cartForm__input {
+  width: 90%;
+  margin: 0 auto;
+  border: 1px solid #000;
+  padding: 13px;
+  box-sizing: border-box;
+  font-size: 0.9rem;
+  font-family: 'Poppins', sans-serif;
+  color: white;
+  font-weight: 400;
+  background: transparent;
+  border: solid 2px white;
+}
+
+.cartForm__input::placeholder {
+  color: #BABABA;
+  opacity: 1;
+}
+
+.cartForm__input:focus {
+  outline: none;
+  border: solid 2px #CE3D3D;
+}
+
+.cartForm__input:-webkit-autofill,
+.cartForm__input:-webkit-autofill:hover, 
+.cartForm__input:-webkit-autofill:focus, 
+.cartForm__input:-webkit-autofill:active{
+    -webkit-box-shadow: 0 0 0 30px #161616 inset !important;
+    -webkit-text-fill-color: white !important;
+    /* cursor white */
+    caret-color: white;
+}
+
+.cartForm__submit {
+  width: 90%;
+  margin: 0 auto;
+  border: none;
+  padding: 18px;
+  box-sizing: border-box;
+  font-size: 1.1rem;
+  font-family: 'Poppins', sans-serif;
+  color: white;
+  font-weight: bold;
+  background: #CE3D3D;
+  border: none;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+.cartForm__submit:hover {
+  box-shadow: 0 0 10px 4px #CE3D3D;
+}
+</style>
--- components/CartItemComponent.vue
+++ components/CartItemComponent.vue
@@ -1,12 +1,15 @@
 <template>
   <article class="cart__item">
-    <img :src="product.img" alt="Product Image">
+    <div class="cart__img">
+      <img :src="product.img" alt="Product Image">
+    </div>
     <div class="cart__itemInfo">
       <h2 class="cartItem__title">{{ product.name }}</h2>
       <p class="cartItem__desc">{{ product.desc }}</p>
       <span class="cartItem__price">${{ product.price }}</span>
       <div class="cartItem__more">
         <button class="cartItem__remove">Remove</button>
+        <span class="cartItem__pipe">|</span>
         <NuxtLink class="cartItem__moreLink" to="/">See More Like This</NuxtLink>
       </div>
     </div>
@@ -17,3 +20,93 @@ export default {
   props: ['product']
 }
 </script>
+<style scoped>
+.cart__item {
+  width: 100%;
+  position: relative;
+  display: flex;
+  margin: 2rem 0;
+}
+
+.cartItem__price {
+  position: absolute;
+  top: 0.4rem;
+  right: 0;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 600;
+  font-size: 1rem;
+  color: white;
+}
+
+.cart__img {
+  width: 50%;
+  max-width: 230px;
+  align-self: flex-start;
+  position: relative;
+}
+
+.cart__img::after {
+  content: "";
+  display: block;
+  padding-bottom: 100%;
+}
+
+.cart__img img {
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.cart__itemInfo {
+  width: 60%;
+  padding-left: 2rem;
+  margin-top: 1.5rem;
+  display: flex;
+  flex-direction: column;
+}
+
+.cartItem__title {
+  font-family: 'League Spartan', sans-serif;
+  font-weight: 600;
+  font-size: 3rem;
+  color: white;
+  margin-bottom: 1rem;
+}
+
+.cartItem__desc {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+  font-size: 0.93rem;
+  color: #BABABA;
+  margin-bottom: 1rem;
+  line-height: 180%;
+}
+
+.cartItem__more {
+  margin-top: auto;
+}
+
+.cartItem__remove, .cartItem__moreLink, .cartItem__pipe {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+  font-size: 0.9rem;
+  color: white;
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  text-decoration: none;
+}
+
+.cartItem__pipe {
+  margin: 0 0.5rem;
+  cursor: default;
+}
+
+.cartItem__remove:hover, .cartItem__moreLink:hover {
+  text-decoration: underline;
+}
+</style>
